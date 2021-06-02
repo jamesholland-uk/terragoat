13fdda389fdde0c3675d7d56720ef94fb76c9ea2
James Holland <jamoiholland@gmail.com>
2021-06-02 15:39:11 +0100

    DB name in lowercase

--- terraform/gcp/big_data.tf
+++ terraform/gcp/big_data.tf
@@ -6,7 +6,7 @@ resource "random_id" "db" {
 }
 
 resource google_sql_database_instance "master_instance" {
-  name                = "terragoat-${var.environment}-master-${random_id.db.id}"
+  name                = lower("terragoat-${var.environment}-master-${random_id.db.id}")
   database_version    = "POSTGRES_11"
   region              = var.region
   deletion_protection = false
